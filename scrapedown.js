2006646f5e669d5effb4d8edcc5a8ecec72e7f77
psd <psd@whatfettle.com>
2013-10-29 09:01:21 +0000

    left and right panes

--- scrapedown.js
+++ scrapedown.js
@@ -22,14 +22,52 @@ function loadScript(src, callback, value) {
 loadScript(scrapedown_location + "readability.js", function () {
     readability.init(function (div) {
         loadScript(scrapedown_location + "to-markdown.js", function () {
-
             var text = toMarkdown(div.innerHTML);
-
-            var textarea = document.createElement("textarea");
+            textarea = scrapedown_build_gui();
             textarea.value = text;
-            textarea.setAttribute('cols', 100);
-            textarea.setAttribute('rows', text.split(/\n/).length);
-            document.body.appendChild(textarea);
         });
     });
 });
+
+
+/*
+ *  create gui
+ */
+function scrapedown_build_gui(text) {
+
+    /*
+    <div id="scrapedown">
+    <div id="leftContainer">
+        <textarea id="inputPane" cols="80" rows="20" class="pane"></textarea> 
+    </div>
+    <div id="rightContainer">
+        <div id="previewPane" class="pane"><noscript><h2>You'll need to enable Javascript to use this tool.</h2></noscript></div>
+    </div>
+    </div>
+    */
+
+    var gui = document.createElement("div");
+    gui.id = "scrapedown";
+
+    var left = document.createElement("div");
+    left.id = "leftContainer";
+    gui.appendChild(left);
+
+    var textarea = document.createElement("textarea");
+    textarea.id = "inputPane";
+    textarea.setAttribute("class", "pane");
+    textarea.setAttribute("cols", 80);
+    textarea.setAttribute("rows", 20);
+    left.appendChild(textarea);
+
+    var right = document.createElement("div");
+    right.id = "rightContainer";
+    gui.appendChild(right);
+
+    var preview = document.createElement("div");
+    preview.setAttribute("class", "pane");
+    preview.id = "previewPane";
+
+    document.body.appendChild(gui);
+    return textarea;
+}
