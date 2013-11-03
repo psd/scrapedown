2610a95b2e9dde6252136d0313e9fe97aaefa338
psd <psd@whatfettle.com>
2013-11-03 19:35:55 +0000

    Normalise spaces

--- to-markdown.js
+++ to-markdown.js
@@ -178,7 +178,7 @@ function toMarkdown(string) {
 
   function replaceBlockquotes(html) {
     html = html.replace(/<blockquote\b[^>]*>([\s\S]*?)<\/blockquote>/gi, function(str, inner) {
-      inner = inner.replace(/^\s+|\s+$/g, '');
+      inner = inner.replace(/^\s*|\s*$/g, '');
       inner = cleanUp(inner);
       inner = inner.replace(/^/gm, '> ');
       inner = inner.replace(/^(>([ \t]{2,}>)+)/gm, '> >');
