e6d1b4d85f3aba1f529c09a158f067411c36f38c
psd <psd@whatfettle.com>
2014-01-06 16:08:58 +0000

    Added polyfill for the Array.map function for IE

--- scrapedown.js
+++ scrapedown.js
@@ -15,6 +15,14 @@ function loadScript(src, callback, value) {
     document.body.appendChild(js);
 }
 
+/*
+ *  polyfill the Array.map function
+ */
+(function(fn){
+    if (!fn.map) fn.map=function(f){var r=[];for(var i=0;i<this.length;i++)r.push(f(this[i]));return r}
+    if (!fn.filter) fn.filter=function(f){var r=[];for(var i=0;i<this.length;i++)if(f(this[i]))r.push(this[i]);return r}
+})(Array.prototype);
+
 scrapedown_init();
 
 /*
